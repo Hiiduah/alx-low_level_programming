ebf3edc98e74c1f598a57f5d1b3b5cf4ab49bade
RWIGAMBA HIRWA Placide <hirwaplacide@gmail.com>
2023-03-16 10:24:01 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * return: 0 (success)
+ * return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
