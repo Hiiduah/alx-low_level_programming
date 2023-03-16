98a20ee21314457e8e251dfe353d7b65b7c1f651
RWIGAMBA HIRWA Placide <hirwaplacide@gmail.com>
2023-03-16 12:57:01 -0700

    quote

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,3 +17,13 @@ int main(void)
 	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
+"6-size.C" [New] 20L 487C written
+root@f4be8d517413:~/alx-low_level_programming/0x00-hello_world# ix
+bash: ix: command not found
+root@f4be8d517413:~/alx-low_level_programming/0x00-hello_world# ls
+0-preprocessor  1-compiler   4-puts.c    a.out   main.c  README.md
+100-intel       2-assembler  5-printf.c  c       main.o  size32
+101-quote.c     3-name       6-size.c    cisfun  main.s  size64
+root@f4be8d517413:~/alx-low_level_programming/0x00-hello_world#
+root@f4be8d517413:~/alx-low_level_programming/0x00-hello_world# chmod a+x 6-size.c
+root@f4be8d517413:~/alx-low_level_programming/0x00-hello_world#
