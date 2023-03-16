6d77724f453bc0d37a6e73f0051c7d45426714b6
RWIGAMBA HIRWA Placide <hirwaplacide@gmail.com>
2023-03-16 09:51:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 /**
-*main - entry point
-*return: always 0 (success)
-*/
+ * main - A program that prints a line with puts function
+ * return: 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
