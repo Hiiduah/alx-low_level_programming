a5f1628abb96c46ad26bea7f3e4d6530170aab1b
RWIGAMBA HIRWA Placide <hirwaplacide@gmail.com>
2023-03-16 10:46:43 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-include < stdio.h >
+#include <stdio.h>
 /**
  * main - A program that prints a line using printf function
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n")
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
