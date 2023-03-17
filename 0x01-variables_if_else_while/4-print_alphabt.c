72b3db67d3f676b7c931df976f68ca4e06bba9bf
RWIGAMBA HIRWA Placide <hirwaplacide@gmail.com>
2023-03-17 16:39:12 -0700

    Missing

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	 char low, e, q;
+	char low, e, q;
 
-	 e = 'e';
-	 q = 'q';
+	e = 'e';
+	q = 'q';
 
-	 for (low = a; low <= z; low++)
-	 {
-		 if (low != e && low != q)
-			 putchar(low);
-	 }
-	 putchar('\n');
-	 return (0);
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	putchar('\n');
+	return (0);
 }
