e635a3097b2b6b79487dbcdd0e69f742085e6ae4
RWIGAMBA HIRWA Placide <hirwaplacide@gmail.com>
2023-03-17 11:07:55 -0700

    Last number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int n;
+        int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-		printf("%d is positive", n);
-	else if (n < 0)
-		printf("%d is negative", n);
-	else if (n == 0)
-		printf("%d is zero", n);
-	return (0);
+        if (n > 0)
+                printf("%d is positive", n);
+        else if (n < 0)
+                printf("%d is negative", n);
+        else if (n == 0)
+                printf("%d is zero", n);
+        return (0);
 }
