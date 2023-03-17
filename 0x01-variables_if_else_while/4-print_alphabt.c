32394bf17676997279a17a451c465f81ae325f04
RWIGAMBA HIRWA Placide <hirwaplacide@gmail.com>
2023-03-17 12:33:39 -0700

    qn4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	 char low, e, q;
+
+	 e = 'e';
+	 q = 'q';
+
+	 for (low = 'a'; low <= 'Z'; low++)
+		 if (low != e && low !=q)
+			 putchar(low);
+	 putchar('\n');
+	 return (0);
+}
