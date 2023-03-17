d9ff548b41dc00e613b7e52f36ec4d9982390e79
RWIGAMBA HIRWA Placide <hirwaplacide@gmail.com>
2023-03-17 10:29:33 -0700

    neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,8 +12,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("Enter the number to check");
-	scanf("%d", &n);
 	if (n > 0)
 		printf("%d is positive", n);
 	else if (n < 0)
