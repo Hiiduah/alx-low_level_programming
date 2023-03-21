7367c2dfc6b32c00cfc9b201c3278f83f97a7f2c
RWIGAMBA HIRWA Placide <hirwaplacide@gmail.com>
2023-03-21 13:56:50 -0700

    putcjar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,9 +1,11 @@
+#include "main.h"
 #include <unistd.h>
 /**
- * putchar - writes the character c in stdout
+ * _putchar - writes the character c in stdout
  * @c: the character to print
+ *
  * Return: on Success 1.
- * on error, -1 is returned and errno is set appropriately
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-int _puchar(char c);
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
 
 void print_alphabet(void);
 
@@ -23,3 +26,5 @@ int add(int, int);
 void print_to_98(int n);
 
 void print_times_table(int n);
+
+#endif
